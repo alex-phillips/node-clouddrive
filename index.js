3470bc19fbb6098af55a4c6e27604ce736fe872d
alex-phillips <ahp118@gmail.com>
2015-10-12 19:03:51 -0400

    proper exporting

---
+++ index.js
@@ -0,0 +1,9 @@
+modele.exports = {
+    Account: require('./lib/Account'),
+    Node: require('./lib/Node'),
+    Cache: {
+        SQLite: require('./lib/Cache/SQLite3'),
+        MySQL: require('./lib/Cache/MySQL'),
+        Mongo: require('./lib/Cache/Mongo')
+    }
+};
