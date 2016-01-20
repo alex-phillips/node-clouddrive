d3396dfdaace2ea577676d9b4ba5b348f5e92448
alex-phillips <ahp118@gmail.com>
2016-01-20 17:34:49 -0500

    exposed additional classes in the module including new  class

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 module.exports = {
   Account: require('./lib/Account'),
   Node: require('./lib/Node'),
+  Config: require('./lib/Config'),
+  Utils: require('./lib/Utils'),
   Cache: {
+    Cache: require('./lib/Cache'),
     SQLite: require('./lib/Cache/SQLite3'),
     MySQL: require('./lib/Cache/MySQL'),
     Mongo: require('./lib/Cache/Mongo')
